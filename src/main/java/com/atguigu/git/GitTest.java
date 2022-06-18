cd217f5d12e4382e6ccbb91f515cfb4c53699eec
zhy <zhy@qq.com>
2022-06-18 18:28:43 +0800

    push commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -9,6 +9,7 @@ public class GitTest {
         System.out.println("hello git4!");
         System.out.println("master text!");
         System.out.println("hot-fix text!");
+        System.out.println("push text!");
     }
 
 }
